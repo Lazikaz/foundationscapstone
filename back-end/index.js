27acd4fd48bb30cc98ac43321a57b2a3cb2316a6
Lazikaz <souflakey@gmail.com>
2022-07-07 17:04:59 -0600

    a

--- back-end/index.js
+++ back-end/index.js
@@ -11,6 +11,8 @@ app.get("/styles", (req, res) => {
     res.sendFile(path.join(__dirname, "../front-end/styles.css"))
 })
 
-
+app.get("/article", (req, res) => {
+    res.sendFile(path.join(__dirname, "../front-end/article.html"))
+})
 
 app.listen(process.env.PORT, () => {console.log("running!")})
