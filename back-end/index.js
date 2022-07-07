555667831bcaac30ac20e7a77ac9f17d7b655d37
Lazikaz <souflakey@gmail.com>
2022-07-07 17:13:46 -0600

    fixed article

--- back-end/index.js
+++ back-end/index.js
@@ -3,6 +3,8 @@ const path = require("path")
 
 const app = express()
 
+//Index html and styles------------------------------------------
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../front-end/index.html"))
 })
@@ -11,10 +13,17 @@ app.get("/styles", (req, res) => {
     res.sendFile(path.join(__dirname, "../front-end/styles.css"))
 })
 
+//---------------------------------------------------------------
+//Article html and styles----------------------------------------
+
 app.get("/article", (req, res) => {
     res.sendFile(path.join(__dirname, "../front-end/article.html"))
 })
 
-app.get("/article")
+app.get("/article/styles", (req, res) => {
+    res.sendFile(path.join(__dirname, "../front-end/articleStyles.css"))
+})
+
+//----------------------------------------------------------------
 
 app.listen(process.env.PORT, () => {console.log("running!")})
