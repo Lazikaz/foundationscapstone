a036e95faf3fb7c725a4c302f08b0e9093623f38
Lazikaz <souflakey@gmail.com>
2022-07-08 21:58:59 -0600

    added article updates to index

--- front-end/index.js
+++ front-end/index.js
@@ -1,7 +1,21 @@
 function getArticles(){
     axios.get("https://capstonebloveall.herokuapp.com/api/index")
     .then(res => {
-        console.log(res)
+        for(i in res.data){
+            let article = document.createElement("li")
+            let list = document.getElementsByName("ul")
+            let image = document.createElement("img")
+            let link = document.createElement("a")
+            let name = document.createElement("h3")
+
+            list.appendChild(article)
+            article.appendChild(link)
+            link.appendChild(image)
+            link.appendChild(name)
+            link.setAttribute("href", `/article/${res.data[i].article_id}`)
+            image.setAttribute("src", `${res.data[i].image}`)
+            name.textContent = `${res.data[i].title}`
+        }
     })
 }
 
